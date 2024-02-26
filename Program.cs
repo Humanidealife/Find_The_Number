ee690501653dcfd5e4614497e2e4dce02fb79e57
qianwang <wangqianlf@live.com>
2024-02-26 23:37:10 +0100

    premier commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Find_The_Number // Remplacez MonNamespace par le nom de votre namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
